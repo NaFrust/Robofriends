6b8834432cf97fbc6c6a3e7bcdada5137aa0d9ca
Nahuel <nahuel.frustaglia@gmail.com>
2021-05-25 13:12:10 -0300

    final con hooks

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React,{ useState, useEffect} from 'react';
 import Cardlist from '../components/CardList.js';
 import SearchBox from '../components/SearchBox.js';
 import Scroll from '../components/Scroll.js';
@@ -6,45 +6,39 @@ import ErrorBoundry from '../components/ErrorBoundry.js'
 import './App.css';
 
 
-class App extends React.Component {
-    constructor(){
-        super();
-        this.state = {
-            robots: [],
-            searchfield: '',
-        }
-    }
-    onSearchChange = (event) => {
-        this.setState({searchfield: event.target.value});
-        }
-        
-componentDidMount(){
-    fetch('https://dummyapi.io/data/api/user?limit=10').then(response => response.json())
-    .then(users => this.setState({robots: users}))
-   
-}
+function App () {
+    const [robots, setRobots] = useState([]);
+    const [searchfield, setSearchfield] = useState('');
+    const [count, setCount] = useState(0);
 
-    render(){
-        const { robots, searchfield } = this.state;
-        const filteredRobots = robots.filter(robot =>{
-            return robot.name.toLowerCase().includes(searchfield.toLowerCase())
-        })
-        return !robots.length ? <h1 className="tc">Cargando</h1> : 
-            (
-                <div className="tc">
-                    <h1 className="f1">RoboFriends</h1>
-                    <SearchBox searchChange={this.onSearchChange}/>
-                    <Scroll>
-                        <ErrorBoundry>
-                            <Cardlist robots={filteredRobots}/>
-                        </ErrorBoundry>
-                    </Scroll>
-                </div>
-            );
-    
-    }
-    
-   
+    const onSearchChange = (event) => {
+        setSearchfield(event.target.value);}
+        
+
+    useEffect(()=>{
+        fetch('https://jsonplaceholder.typicode.com/users/')
+        .then(response => response.json())
+        .then(users => setRobots(users)) 
+        console.log(count)
+        },[count]); //va a funcionar cada vez que count cambie
+         
+    const filteredRobots = robots.filter((robot) => {
+        return (robot.name.toLowerCase().includes(searchfield.toLowerCase()))
+        });
+
+    return !robots.length ? <h1 className="tc">Cargando</h1> : 
+        (
+            <div className="tc">
+                <h1 className="f1">RoboFriends</h1>
+                <button onClick={()=>setCount(count+1)}>click me</button>
+                <SearchBox searchChange={onSearchChange}/>
+                <Scroll>
+                    <ErrorBoundry>
+                        <Cardlist robots={filteredRobots}/>
+                    </ErrorBoundry>
+                </Scroll>
+            </div>
+        );
 }
 
 export default App;
